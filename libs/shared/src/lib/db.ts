82ec003b5f2b9b47bef70fdd55fd63ec2558a1eb
quitovsky <quitovsky@vk.com>
2025-04-26 15:01:40 +0300

    remove unused function

--- libs/shared/src/lib/db.ts
+++ libs/shared/src/lib/db.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
-
-export async function createUserIfNotExists(telegramId: string) {
-    await prisma.user.upsert({
-        where: { telegramId },
-        create: { telegramId },
-        update: {}
-    })
-}
